968047f27e36f028b7d5a11cc33a32d3252ab7cb
adrianchansp <88935247+adrianchansp@users.noreply.github.com>
2025-03-23 20:10:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
 export default {
   async fetch(request, env, ctx) {
   
@@ -35,4 +45,4 @@ export default {
 	
     return new Response("Not found", { status: 404 });
   }
-}
+};
